2ed6bfdd2bc130c4eec7b60bdc9f4c461d5a7625
Will Han <xingheng907@hotmail.com>
2018-05-10 16:43:02 +0800

    Initial Commit

---
+++ Reminders Exporter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Reminders Exporter
+//
+//  Created by WeiHan on 10/05/2018.
+//  Copyright © 2018 WillHan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
